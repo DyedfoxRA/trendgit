be1ad73c514d85a0367dac2dc4c80413f40a1390
Andrii <dyedfox.ra@gmail.com>
2024-06-22 22:48:06 +0200

    add koin to main module

---
+++ app/src/main/java/com/venture/trendgit/app/TrendGitApp.kt
@@ -0,0 +1,18 @@
+package com.venture.trendgit.app
+
+import android.app.Application
+import com.venture.network.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class TrendGitApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@TrendGitApp)
+            modules(networkModule)
+        }
+    }
+}
