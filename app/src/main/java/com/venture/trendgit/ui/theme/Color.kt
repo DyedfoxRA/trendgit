0a6c52ffdbf318779a4fe0f555e923acd7308fd9
Andrii <dyedfox.ra@gmail.com>
2024-06-23 23:06:19 +0200

    add new colors

--- app/src/main/java/com/venture/trendgit/ui/theme/Color.kt
+++ app/src/main/java/com/venture/trendgit/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.venture.trendgit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBlue = Color(0xFFE6F3FF)
+val Blue = Color(0xFF007AFF)
+val DarkBlue = Color(0xFF005BB5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val LightGray = Color(0xFFF2F2F2)
+val Gray = Color(0xFFCCCCCC)
+val DarkGray = Color(0xFF666666)
+
+val LightBackground = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF121212)
+
+val LightSurface = Color(0xFFFFFFFF)
+val DarkSurface = Color(0xFF1E1E1E)
+
+val LightOnPrimary = Color(0xFFFFFFFF)
+val DarkOnPrimary = Color(0xFF000000)
+
+val Pink80 = Color(0xFFEFB8C8)
 val Pink40 = Color(0xFF7D5260)
